da4eda1dd3ec68f962aedcfb3818121ce31f9bb6
t1633361 <t163361@126.com>
2020-09-22 16:49:03 +0800

    First Sample
    1.First Sample

---
+++ UGUI Layout Sample/Assets/Scripts/LayoutSample1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LayoutSample1 : MonoBehaviour
+{
+    public GameObject LeftAreaRoot;
+    public GameObject RightAreaRoot;
+    public GameObject DownAreaRoot;
+    
+    public Button LeftButton;
+    public Button RightButton;
+    public Button DownButton;
+
+
+
+    public void EnableLeftArea()
+    {
+        LeftAreaRoot.SetActive(!LeftAreaRoot.activeSelf);
+    }
+    public void EnableRightArea()
+    {
+        RightAreaRoot.SetActive(!RightAreaRoot.activeSelf);
+    }
+    public void EnableDownArea()
+    {
+        DownAreaRoot.SetActive(!DownAreaRoot.activeSelf);
+    }
+}
